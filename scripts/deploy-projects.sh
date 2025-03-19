f40425c8caac03c2d27ba7cd83fc8a64158b273d
YarLama <gaideichuk1998@mail.ru>
2025-03-20 00:41:23 +0300

    workflow #2

--- scripts/deploy-projects.sh
+++ scripts/deploy-projects.sh
@@ -3,11 +3,15 @@
 changed_files=$(git diff --name-only HEAD^ HEAD)
 projects=$(find . -maxdepth 1 -type d -name 'project*' | sed 's|./||')
 changed_projects=""
+
+echo $changed_files
 for project in $projects; do
   if echo "$changed_files" | grep -q "^$project/"; then
     changed_projects="$changed_projects $project"
   fi
 done
+
+echo $changed_projects
 
 if [ -z "$changed_projects" ]; then
   echo "No projects change. Skipping."
